269f892930d3c8f4a1b76f9041c0a3748849bbbe
Gregory Grillone <greg.grillone@gmail.com>
2014-11-08 16:01:42 -0500

    Add app name to environment config files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+PersonalSite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
