e70286bf7b48a594afcca772b36c4f430f8b0ed0
Gregory Grillone <greg.grillone@gmail.com>
2015-01-12 20:28:34 -0500

    Implement admin auditing for social media create, update and destroy methods.

--- app/admin/social_media.rb
+++ app/admin/social_media.rb
@@ -4,6 +4,47 @@ ActiveAdmin.register SocialMedia do
   config.filters = false
   config.sort_order = 'name_asc'
 
+  controller do
+    def create
+      @social_media = SocialMedia.new(permitted_params[:social_media])
+
+      if @social_media.save
+        AuditLog.create(AdminUserAudit, current_admin_user.id, request, params, 'admin_user_audit_log_blacklist.yml', nil, nil)
+        flash[:notice] = "Social media was successfully created."
+        redirect_to admin_social_media_path
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      @social_media = SocialMedia.find(params[:id])
+      original_attrs = @social_media.attributes
+
+      if @social_media.update(permitted_params[:social_media])
+        new_attrs = @social_media.attributes
+        AuditLog.create(AdminUserAudit, current_admin_user.id, request, params, 'admin_user_audit_log_blacklist.yml', original_attrs, new_attrs)
+        flash[:notice] = 'Social media was successfully updated.'
+        redirect_to admin_social_media_path
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @social_media = SocialMedia.find(params[:id])
+
+      if @social_media.destroy
+        AuditLog.create(AdminUserAudit, current_admin_user.id, request, params, 'admin_user_audit_log_blacklist.yml', nil, nil)
+        flash[:notice] = "Social media was successfully deleted."
+        redirect_to admin_social_media_path
+      else
+        flash[:alert] = "Social media failed to delete."
+        redirect_to :back
+      end
+    end
+  end
+
   index do
     selectable_column
 
